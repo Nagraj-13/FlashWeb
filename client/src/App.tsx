4daf37fa018f6c4170bf69a2cb4ce1628acdf17a
OneBoatSolutions <nagrajy131200350@gmail.com>
2025-04-03 12:18:40 +0530

    vercel analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import Home from "./components/Home";
-
+import { Analytics } from '@vercel/analytics/react';
 function App() {
     return (
         <div className="">
             <Home />
+            <Analytics />
         </div>
     );
 }
